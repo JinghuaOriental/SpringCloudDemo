b9e05e1e2e5018c0ca6f7233e72b2d3d5aea12ff
JinghuaOriental <1795185859@qq.com>
2024-08-28 22:14:18 +0800

    P14 Gateway：路由网关

---
+++ gateway-service/src/main/java/org/example/GatewayApplication.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Aaso
+ * @version 1.0
+ */// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
